f7ad0cd149c6e88bc613d217e79e38f00c6f7a09
kevin-cueva <unimagneill@gmail.com>
2021-12-10 00:02:05 -0500

    otra modificacion

--- areaContable/areaContable/urls.py
+++ areaContable/areaContable/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('areaContableApp.urls')),
     path('', include('constitucion_de_empresa.urls')),
     path('', include('normatividad_contable.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
